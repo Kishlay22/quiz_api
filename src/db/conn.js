0bd7a2cd985dabe8cdbaf7591546289b6c8fa766
Kishlaykumar <75835349+Kishlay22@users.noreply.github.com>
2022-12-07 14:30:35 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-const db='mongodb+srv://quiz_app:nis%40quiz@cluster0.louo2t1.mongodb.net/quiz_details?retryWrites=true&w=majority';
+const db='//mongodb url for the database ';
 mongoose.connect(db,{
     useNewUrlParser: true, useUnifiedTopology: true
  }).then(()=>{
